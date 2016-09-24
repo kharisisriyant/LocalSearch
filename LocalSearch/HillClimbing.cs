a03fc66611c02a2d896e0de0db8843657ab4df40
Albert L <Albert L>
2016-09-24 11:14:37 +0700

    Added HillClimbing File

---
+++ LocalSearch/HillClimbing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LocalSearch
+{
+	public class HillClimbing
+	{
+		public HillClimbing (List<MataKuliah> listMK, List<Ruangan> listR, int banyakJadwal, int banyakRuangan)
+		{
+
+		}
+
+        
+	}
+}
